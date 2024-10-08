a12688cce45f61143a5584ccd6241bdf25d42210
suren-11 <presanth11@gmail.com>
2024-10-08 11:10:22 +0530

    defined a student class

---
+++ model/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplicationTraining2.model
+{
+    public class Student
+    {
+        public string Title { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string SubjectStream { get; set; }
+        public int JoinYear { get; set; }
+        public string Gender { get; set; }
+    }
+}
